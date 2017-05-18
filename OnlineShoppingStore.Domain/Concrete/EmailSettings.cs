1c69e0afa25ba5d0817bb9156e52a35bcd863328
ChandaraLeang <leangchandara@gmail.com>
2017-05-17 23:59:03 -0500

    Initial Commit

---
+++ OnlineShoppingStore.Domain/Concrete/EmailSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShoppingStore.Domain.Concrete
+{
+    public class EmailSettings
+    {
+        public string MailToAddress = "leangchandara@gmail.com";
+        public string MailFromAddress = "leangchandara@gmail.com";
+        public bool UseSsl = true;
+        public string Username = "leangchandara@gmail.com";
+        public string Password = "apyhljruobdirfzb";
+        public string ServerName = "smtp.gmail.com";
+        public int ServerPort = 587;
+    }
+}
---
+++ OnlineShoppingStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,55 @@
+﻿using OnlineShoppingStore.Domain.Abstract;
+using OnlineShoppingStore.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShoppingStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository repository;
+
+        //Declare the number of row that will show for each page
+        public int PageSize = 4;
+
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        //View the number of row that will show for each page
+        public ViewResult List(string category, int page = 1)
+        {
+            //return View(repository.Products
+            //            .OrderBy(p => p.ProductId)
+            //            .Skip((page - 1) * PageSize)
+            //            .Take(PageSize)
+            //    );
+
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = repository.Products
+                    .Where(p => category == null || p.Category == category)
+                    .OrderBy(p => p.ProductId)
+                    .Skip((page - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    //TotalItems = repository.Products.Count()
+
+                    //update view by category
+                    TotalItems = category == null ?
+                                 repository.Products.Count() :
+                                 repository.Products.Where(p => p.Category == category).Count()
+                },
+                CurrentCategory = category
+            };
+            return View(model);
+        }
+    }
+}
